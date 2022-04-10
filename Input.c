1d9003b86f585b2aec3d73eb851a1dd866d200f9
Somendra Pratap <somendra.pratapsingh.15@gmail.com>
2022-04-10 21:41:02 +0530

    Initial commit

---
+++ Input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a and b : ");
+    // Space or Return separated
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("The Sum is : %d\n",a+b);
+    return 0;
+}
